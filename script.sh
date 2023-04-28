e549b1d4a3353423dd480994d60f7697049b9568
Yaroslav <yaroslav@MacBook-Pro.local>
2023-04-28 11:05:59 +0300

    Additions

---
+++ script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+	cd folder1
+		mkdir fold_1 fold_2 fold_3
+                    cd fold_2
+                touch 11.txt 22.txt 33.txt 44.json 55.json
+            mkdir f_1 f_2 f_3
+        ls -la
+    mv 11.txt 22.txt f_1
